843bdc6b31f633298b2408156836864fa0014550
Vijay6868 <saroyav3@gmail.com>
2023-12-12 06:29:20 +1300

    dictionaries file added

---
+++ dictionaries_.py
@@ -0,0 +1,11 @@
+mydict = {"name":"Vijay", "age":23, "city":"Auckland"}
+print(mydict)
+
+mydict["email"]="saroyav3@gmail.com"
+print(mydict)
+
+mydict.popitem()
+print(mydict)
+
+#for key, value in mydict.itmes():
+    #print(key,value)
